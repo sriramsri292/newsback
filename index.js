cad66239b5bece28045b2ac70c3ea28a016f51a1
sriramsri292 <133184406+sriramsri292@users.noreply.github.com>
2023-11-30 16:12:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const HTTP_SERVER = express();
 
 
 const PORT =  5000;
+
+HTTP_SERVER.use(cors());
 
 HTTP_SERVER.use("/news",require("./controllers/news"));
 HTTP_SERVER.listen(PORT, "0.0.0.0", (err) => {
